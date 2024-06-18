9a2798e9122ab3a61a46726de189e5828d581d19
~hedy <hedy.dev@protonmail.com>
2024-06-18 13:41:15 +0800

    improvement

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 function love.load()
     love.graphics.setNewFont(20)
     ROWS = 5
-    Message = "hello"
+    Message = "Press a number key 1 to 5"
     Grid = {}
     --- @type number[] Next index for insertion
     GridTops = {}
+    Next = 2
     for y = 1, ROWS do
         Grid[y] = {}
         GridTops[y] = 1
@@ -17,6 +18,8 @@ end
 function love.draw()
     local tile_width = 95
     local tile_color = {54/256, 58/256, 78/256}
+    local tile_padding = {15, math.floor(tile_width / 2 - 15)}
+
     local row = 10
     for y = 1, ROWS do
         local col = 10
@@ -25,15 +28,16 @@ function love.draw()
             love.graphics.rectangle('fill', col, row, tile_width, tile_width)
             love.graphics.setColor(1, 1, 1)
             if Grid[y][x] > 0 then
-                love.graphics.print(tostring(Grid[y][x]), col + 4, row + 4)
+                love.graphics.print(tostring(Grid[y][x]), col + tile_padding[1], row + tile_padding[2])
             end
             col = col + tile_width + 10
         end
         row = row + tile_width + 10
     end
+
     love.graphics.setColor(1, 1, 1)
     row = row + 10
-    love.graphics.print("Next: 8", 10, row)
+    love.graphics.print("Next: " .. tostring(Next), 10, row)
     row = row + 25
     love.graphics.print(Message, 10, row)
 end
@@ -43,7 +47,7 @@ function love.keypressed(key)
     if col and col > 0 and col <= ROWS then
         local row = GridTops[col]
         if row <= ROWS then
-            Grid[row][col] = 8
+            Grid[row][col] = Next
             GridTops[col] = row + 1
             Message = "Yay"
         else
