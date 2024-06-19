f9e84842e49a7ab490280b3c8552cb16aae15ede
~hedy <hedy.dev@protonmail.com>
2024-06-19 12:42:22 +0800

    COLORS!

--- main.lua
+++ main.lua
@@ -3,12 +3,51 @@ function love.load()
     ROWS = 5
     Message = "Press a number key 1 to 5"
     Grid = {}
-    --- @type number[] Next index for insertion, actually index by x.
+    ---@alias rgb number[]
+    ---@type number[] Next index for insertion, actually index by x.
     GridTops = {}
     Next = 2
     Max = 2
     Count = 0
     State = "begin"
+
+    ---@alias hex string
+    ---@alias color hex|rgb
+    ---@alias colorInfo {bg: color, fg: boolean|rgb}
+    ---{ HEX, is-fg? }
+    ---@type { [number]: colorInfo }
+    ColorPalette = {
+        [2]   = {bg = "473335", fg = true},
+        [4]   = {bg = "4E5D5E", fg = true},
+        [8]   = {bg = "548687", fg = true},
+        [16]  = {bg = "A89877", fg = false},
+        [32]  = {bg = "FCAA67", fg = false},
+        [64]  = {bg = "9582FF", fg = false},
+        [128] = {bg = "2165BF", fg = true},
+        [256] = {bg = "43CDC6", fg = false},
+        [512] = {bg = "C996B3", fg = false},
+        [1024] = {bg = "C95270", fg = false},
+    }
+    FallbackColor = {bg = {1, 1, 1}, fg = {0, 0, 0}}
+    -- Convert hex to rgb
+    for _, v in pairs(ColorPalette) do
+        ---@type rgb
+        local rgb = {}
+        local hex = v.bg
+        -- A little less cryptic than `for i = 1, 5, 2 do`
+        for _, i in ipairs({1, 3, 5}) do
+            ---@cast hex string
+            rgb[#rgb + 1] = tonumber(hex:sub(i, i+1), 16) / 256
+            i = i + 1
+        end
+        ---@as rgb
+        v.bg = rgb
+        ---@as rgb
+        v.fg = v.fg and {1, 1, 1} or {0, 0, 0}
+    end
+    ColorPalette[0] = {bg = {54/256, 58/256, 78/256}, fg = {1, 1, 1}}
+
+    ---@type { [number]: number }
     NextMap = {
         [2] = 2,
         [4] = 2,
@@ -52,13 +91,13 @@ function love.load()
 
     function UpdateNext()
         local cap = NextMap[Max] or NextTooHigh
-        Next = 2 ^ math.random(math.log(cap))
+        -- Next = 2 ^ math.random(math.log(cap))
+        Next = cap
     end
 end
 
 function love.draw()
     local tile_width = 90
-    local tile_color = {54/256, 58/256, 78/256}
     local tile_padding = {15, math.floor(tile_width / 2 - 15)}
 
     local row = 10
@@ -66,9 +105,15 @@ function love.draw()
     for y = 1, ROWS do
         local col = 10
         for x = 1, ROWS do
-            love.graphics.setColor(unpack(tile_color))
+            local fg = (ColorPalette[Grid[y][x]] or FallbackColor).fg
+            local bg = (ColorPalette[Grid[y][x]] or FallbackColor).bg
+
+            ---@cast bg rgb
+            love.graphics.setColor(unpack(bg))
             love.graphics.rectangle('fill', col, row, tile_width, tile_width)
-            love.graphics.setColor(1, 1, 1)
+
+            ---@cast fg rgb
+            love.graphics.setColor(unpack(fg))
             if Grid[y][x] > 0 then
                 Count = Count + 1
                 love.graphics.print(
